94bccf30af937ec53263532284b26e9a853766c1
zman43g <zman3@yandex.ru>
2025-09-19 10:32:25 +0700

    JavaCore HW4 изменения внесены

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
-    private final CalculatorService calculatorService = new CalculatorService();
+    private final CalculatorService calculatorService;
 
-    /*public CalculatorController(CalculatorService calculatorService) {
+    public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
-    }*/
+    }
 
     @GetMapping(path = "/calculator")
     public String firstPage() {
@@ -24,17 +24,17 @@ public class CalculatorController {
     }
 
     @GetMapping(path = "calculator/minus")
-    public String num1MinusNum2(@RequestParam(value = "num1",required = false) Integer number1, @RequestParam(value = "num2",required = false) Integer number2) {
+    public String num1MinusNum2(@RequestParam(value = "num1", required = false) Integer number1, @RequestParam(value = "num2", required = false) Integer number2) {
         return calculatorService.minusOutput(number1, number2);
     }
 
     @GetMapping(path = "calculator/multiply")
-    public String num1MultiplyNum2(@RequestParam(value = "num1",required = false) Integer number1, @RequestParam(value = "num2",required = false) Integer number2) {
+    public String num1MultiplyNum2(@RequestParam(value = "num1", required = false) Integer number1, @RequestParam(value = "num2", required = false) Integer number2) {
         return calculatorService.multiplyOutput(number1, number2);
     }
 
     @GetMapping(path = "calculator/divide")
-    public String num1DivideNum2(@RequestParam(value = "num1",required = false) Integer number1, @RequestParam(value = "num2",required = false) Integer number2) {
+    public String num1DivideNum2(@RequestParam(value = "num1", required = false) Integer number1, @RequestParam(value = "num2", required = false) Integer number2) {
         return calculatorService.divideOutput(number1, number2);
     }
 
